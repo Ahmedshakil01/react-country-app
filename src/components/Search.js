6def5076a86cd809c2f19bcfd7ab5773fa10ce13
shakil ahmed <shakil@aleshatech.com.bd>
2022-10-08 19:43:49 +0600

    react-country-app

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react'
+
+const Search = (props) => {
+    const [searchText, setSearchText] = useState("")
+    const handleChange = (event) =>{
+        setSearchText(event.target.value);
+    } 
+    useEffect(()=>{
+        props.onSearch(searchText);
+      }, [searchText])
+  return (
+    <div style={{textAlign:"center"}}>
+      <input type="text" placeholder = 'serch Country' value={searchText} onChange={handleChange}/>
+    </div>
+  )
+}
+
+export default Search
